b01a2f7c9989f51991e20f0a08a16533febb7ca2
Allan Strand <stranda@gmail.com>
2016-02-16 16:53:43 -0500

    still working on genind

--- R/landscape.make.genind.R
+++ R/landscape.make.genind.R
@@ -1,9 +1,11 @@
 #Create genind object from landscape
-
+#ignores the haploid loci
 landscape.make.genind <- function(Rland)
   {
-      tab <- 2*landscape.ind.freq(Rland)
+      tab <- landscape.ind.freq(Rland)*2
       dimnames(tab) <- list(rownames=1:dim(tab)[1],colnames=landscape.freq.locnames(Rland))
+      pl <- landscape.ploidy(Rland)
       populations <- landscape.populations(Rland)
-      genind(tab,pop=as.factor(populations),ploidy=2)
+      gi=genind(tab,pop=as.factor(populations),ploidy=2)
+      gi[,loc=which(pl>1)]
   }
