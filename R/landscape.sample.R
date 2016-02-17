f1dfdddc120b9e1cc4b5f1c9b3f938bd8044a3c9
Allan Strand <stranda@gmail.com>
2016-02-17 10:15:40 -0500

    repaired landscape.sample so it can deal with empty populations

--- R/landscape.sample.R
+++ R/landscape.sample.R
@@ -42,10 +42,14 @@ landscape.sample.pops <- function(Rland,ns=NULL,pvec=NULL)
                                 inds <- Rland$individuals[landscape.populations(Rland)==p,]
                                 Rland$individuals <-  Rland$individuals[landscape.populations(Rland)!=p,]
                                 inddim <- dim(inds)[1]
-                                if (inddim<ns)
-                                    Rland$individuals <- rbind(Rland$individuals,inds)
-                                else
-                                    Rland$individuals <- rbind(Rland$individuals,inds[sample(1:inddim,ns,replace=F),])
+                                if (!is.null(inddim))
+                                    {
+                                        if (inddim<ns)
+                                            Rland$individuals <- rbind(Rland$individuals,inds)
+                                        else
+                                            Rland$individuals <- rbind(Rland$individuals,inds[sample(1:inddim,ns,replace=F),])
+                                    }
+                            
                             }
                         Rland$individuals <- Rland$individuals[order(Rland$individuals[,1],Rland$individuals[,4]),]
                     }
