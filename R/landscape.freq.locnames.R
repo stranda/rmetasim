864e9e99cc5c8d51b519a5e5319675d4ed92385f
test account <stranda@gmail.com>
2016-03-28 20:13:07 -0400

    Added help pages for a couple of new functions

--- R/landscape.freq.locnames.R
+++ R/landscape.freq.locnames.R
@@ -1,18 +1,13 @@
-#' @title Landscape locus names
-#' @description Landscape locus names
-#'
-#' @param l a \code{rmetasim} landscape object.
-#'
-#'
-landscape.freq.locnames <- function(l)
+landscape.freq.locnames <- function(Rland)
   {
-    num.loc <- length(landscape.ploidy(l))
-    namevec <- NULL
-    for (loc in 1:num.loc)
+      l <- Rland
+      num.loc <- length(landscape.ploidy(l))
+      namevec <- NULL
+      for (loc in 1:num.loc)
       {
-        genos <- landscape.locus(l,loc)[,-1:-landscape.democol()]
-        loc.names <- paste(loc, names(table(unlist(genos))), sep = ".")
-        namevec <- c(namevec,paste("L", loc.names, sep = '')) #not a fast construct, I know.  But remember Knuth "early optimization is the root of all
+          genos <- landscape.locus(l,loc)[,-1:-landscape.democol()]
+          loc.names <- paste(loc, names(table(unlist(genos))), sep = ".")
+          namevec <- c(namevec,paste("L", loc.names, sep = '')) #not a fast construct, I know.  But remember Knuth "early optimization is the root of all
       }
-    namevec
+      namevec
   }
