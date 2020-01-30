c2f5551a864e6b358dd6bb1ed54df307ab022416
Eric Archer <eric.archer@noaa.gov>
2020-01-30 02:42:58 -0800

    speed improvements and bug fixes

--- R/landscape.make.genind.R
+++ R/landscape.make.genind.R
@@ -1,11 +1,15 @@
 #Create genind object from landscape
 #ignores the haploid loci
-landscape.make.genind <- function(Rland)
-  {
-      tab <- landscape.ind.freq(Rland)*2
-      dimnames(tab) <- list(rownames=1:dim(tab)[1],colnames=landscape.freq.locnames(Rland))
-      pl <- landscape.ploidy(Rland)
-      populations <- landscape.populations(Rland)
-      gi=genind(tab,pop=as.factor(populations),ploidy=2)
-      gi[,loc=which(pl>1)]
-  }
+landscape.make.genind <- function(Rland) {
+  tab <- landscape.ind.freq(Rland) * 2
+  dimnames(tab) <- list(
+    rownames = 1:dim(tab)[1],
+    colnames = landscape.freq.locnames(Rland)
+  )
+  gi <- adegenet::genind(
+    tab,
+    pop = as.factor(landscape.populations(Rland)),
+    ploidy = 2
+  )
+  gi[, loc = which(landscape.ploidy(Rland) > 1)]
+}
